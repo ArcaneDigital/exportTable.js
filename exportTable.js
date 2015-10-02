2ef2de41d177c9da459cbc1a736146984890b178
Jay Goodfellow <j.goodfellow@arcane.ws>
2015-10-02 10:34:01 -0400

    added js style linter and futzed with code until it passed

--- exportTable.js
+++ exportTable.js
@@ -1,61 +1,56 @@
+'use strict';
 var exportTable = {
-	init: function(options){
-		var options = options || {};
-		options.format =	options.format || 'csv';
-		options.filename = options.filename || 'export.csv';
+    init: function(o) {
+        var options = o || {};
+        options.format =  options.format || 'csv';
+        options.filename = options.filename || 'export.csv';
+        var items = document.querySelectorAll(options.initiator);
+        var initiator = this.castNodeList(items);
+        initiator.forEach(this.bindEvent.bind(this));
+        exportTable.options = options;
+    },
 
-		var initiator = this.castNodeList(document.querySelectorAll(options.initiator));
-		initiator.forEach(this.bindEvent.bind(this));
+    castNodeList: function(list) {
+        return [].slice.call(list);
+    },
 
-		exportTable.options = options;
-	},
+    bindEvent: function(el) {
+        el.addEventListener('click', this.export, false);
+    },
 
-	castNodeList: function (list) {
-		return [].slice.call(list);
-	},
+    data: function(selector) {
+        var returnValue = '';
+        var table = document.querySelector(selector);
+        var head = table.getElementsByTagName('thead');
+        if (head.length) {
+            var headRows = head[0].children;
+            var headCells = headRows[0].children;
+            for (var i = 0; i < headCells.length; i++) {
+                returnValue += '"' + headCells[i].textContent + '",';
+            }
+            returnValue +=  '\n';
+        }
 
-	bindEvent: function (el) {
-		el.addEventListener('click', this.export, false);
-	},
+        var body = table.getElementsByTagName('tbody')[0];
+        var bodyRows = body.children;
 
-	data: function(selector) {
-		var returnValue = '';
+        for (var i = 0; i < bodyRows.length; i++) {
+            var bodyCells = bodyRows[i].children;
+            for (var j = 0; j < bodyCells.length; j++) {
+                returnValue += '"' + bodyCells[j].textContent + '",';
+            }
+            returnValue +=  '\n';
+        }
+    },
 
-		var table = document.querySelector(selector);
-
-		var head = table.getElementsByTagName('thead');
-		if(head.length){
-			var headRows = head[0].children;
-		    var headCells = headRows[0].children;
-		    for(var i=0;i<headCells.length;i++){
-		    	returnValue += '"' + headCells[i].textContent + '",';
-		    }	
-		    returnValue +=  '\n';		
-		}
-
-		var body = table.getElementsByTagName('tbody')[0];
-		var bodyRows = body.children;
-		
-		for(var i=0;i<bodyRows.length;i++){
-		    var bodyCells = bodyRows[i].children;
-		    for(var j=0;j<bodyCells.length;j++){
-		    	returnValue += '"' + bodyCells[j].textContent + '",';
-		    }
-		    returnValue +=  '\n';
-		} 
-
-		return returnValue;
-	},
-
-	export: function() {
-		data = exportTable.data(this.getAttribute('data-export'));
-		csvData = 'data:application/csv;charset=utf-8,' + encodeURIComponent(data);
-		this.setAttribute('download', exportTable.options.filename);
-		this.setAttribute('target', '_blank');
-		this.setAttribute('href', csvData);
-	},
-
-
+    export: function() {
+        data = exportTable.data(this.getAttribute('data-export'));
+        csvData = 'data:application/csv;charset=utf-8,';
+        csvData += encodeURIComponent(data);
+        this.setAttribute('download', exportTable.options.filename);
+        this.setAttribute('target', '_blank');
+        this.setAttribute('href', csvData);
+    },
 };
 
 window.exportTable = exportTable;
